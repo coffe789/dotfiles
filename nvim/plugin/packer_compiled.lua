416269781ea71e884dec3c7638d46bfa5b309146
Jonathan Davey <jonathan.davey@skykraft.com.au>
2024-01-05 14:02:47 +1100

    Added zshrc

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/coffe789/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/coffe789/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/coffe789/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/coffe789/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/coffe789/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/home/jonathand/.cache/nvim/packer_hererocks/2.1.1702233742/share/lua/5.1/?.lua;/home/jonathand/.cache/nvim/packer_hererocks/2.1.1702233742/share/lua/5.1/?/init.lua;/home/jonathand/.cache/nvim/packer_hererocks/2.1.1702233742/lib/luarocks/rocks-5.1/?.lua;/home/jonathand/.cache/nvim/packer_hererocks/2.1.1702233742/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/jonathand/.cache/nvim/packer_hererocks/2.1.1702233742/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,57 +76,57 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["auto-session"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/auto-session",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/auto-session",
     url = "https://github.com/rmagatti/auto-session"
   },
   ["bufferline.nvim"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/bufferline.nvim",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/bufferline.nvim",
     url = "https://github.com/akinsho/bufferline.nvim"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-cmdline"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/cmp-cmdline",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/cmp-cmdline",
     url = "https://github.com/hrsh7th/cmp-cmdline"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-nvim-lua"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
     url = "https://github.com/hrsh7th/cmp-nvim-lua"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   colorschemes = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/colorschemes",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/colorschemes",
     url = "https://github.com/lunarvim/colorschemes"
   },
   ["friendly-snippets"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   ["live-server.nvim"] = {
@@ -134,92 +134,92 @@ _G.packer_plugins = {
     loaded = false,
     needs_bufread = false,
     only_cond = false,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/opt/live-server.nvim",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/opt/live-server.nvim",
     url = "https://github.com/aurum77/live-server.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["neovim-remote"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/neovim-remote",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/neovim-remote",
     url = "https://github.com/mhinz/neovim-remote"
   },
   ["null-ls.nvim"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
     url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lastplace"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/nvim-lastplace",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/nvim-lastplace",
     url = "https://github.com/ethanholz/nvim-lastplace"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/kyazdani42/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["popup.nvim"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/popup.nvim",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/popup.nvim",
     url = "https://github.com/nvim-lua/popup.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["tokyonight.nvim"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
   },
   ["vim-be-good"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/vim-be-good",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/vim-be-good",
     url = "https://github.com/ThePrimeagen/vim-be-good"
   },
   ["vim-godot"] = {
     loaded = true,
-    path = "/home/coffe789/.local/share/nvim/site/pack/packer/start/vim-godot",
+    path = "/home/jonathand/.local/share/nvim/site/pack/packer/start/vim-godot",
     url = "https://github.com/habamax/vim-godot"
   }
 }
---
+++ zsh/.zshrc
@@ -0,0 +1,27 @@
+# Use powerline
+USE_POWERLINE="true"
+# Has weird character width
+# Example:
+#    is not a diamond
+HAS_WIDECHARS="false"
+# Source manjaro-zsh-configuration
+if [[ -e /usr/share/zsh/manjaro-zsh-config ]]; then
+  source /usr/share/zsh/manjaro-zsh-config
+fi
+# Use manjaro zsh prompt
+if [[ -e /usr/share/zsh/manjaro-zsh-prompt ]]; then
+  source /usr/share/zsh/manjaro-zsh-prompt
+fi
+
+# Aliases
+alias gst="git status"
+alias gp="git push"
+alias gl="git pull"
+alias nv="nvim"
+alias gc="git checkout"
+
+# Fuzzy completion
+zstyle ':completion:*' matcher-list '' \
+  'm:{a-z\-}={A-Z\_}' \
+  'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
+  'r:|?=** m:{a-z\-}={A-Z\_}'
